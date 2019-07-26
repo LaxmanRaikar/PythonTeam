5452b0fd77d2436eac1735349b38ea695f318a57
sagarmundkar <shreemundkar@gmail.com>
2019-07-26 15:39:31 +0530

    serializer class added

---
+++ pythonteam/myteam/models.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+
+class UserProfileInfo(models.Model):
+	""" This class make the model of user and create new model in database """
+	# define user field
+	user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+
+	# define image field in database
+	image = models.ImageField(upload_to='Images', blank=True, null=True)
+
+	def __str__(self):
+		return self.user.username
---
+++ pythonteam/myteam/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .models import UserProfileInfo
+# Create your views here.
+
+
+def login(request):
+    return render('login.html')
+
+
+def sing_up(request):
+    username = request.data.get("user")
+    image = request.FILES['image']
+    user = UserProfileInfo.objects.create(user=username, image=image)
+    return HttpResponseRedirect('login', user)
+
+def CreateNote(CreateAPIView):
+    serializer_class = NoteSerializer
+    
